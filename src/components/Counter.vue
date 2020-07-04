f03239eceba8e43062a31b9b70b7ba2c689af095
yataka-ngnl <59716648+yataka-ngnl@users.noreply.github.com>
2020-07-04 18:37:29 +0900

    カウンターと商品リストの追加

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Counter
+    <div>{{ countMessage }}</div>
+    <button @click="count++">クリック！</button>
+    <button @click="count = 0">リセット！</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    countMessage() {
+      return "回数: " + this.count;
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item" :class="{ over500: item.price >= 500 }">
+        <div class="name">名前: {{ item.name }}</div>
+        <div class="price">{{ item.price }} 円</div>
+        <div v-if="item.price >= 10000">高額商品</div>
+      </div>
+    </div>
+    <div>
+      <label>
+        名前
+        <input type="text" v-model="newItemName" />
+      </label>
+      <label>
+        価格
+        <input type="number" v-model="newItemPrice" />
+      </label>
+      <button @click="addItem">add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+  data() {
+    return {
+      items: [
+        { name: "たまご", price: 100 },
+        { name: "りんご", price: 160 }
+      ],
+      newItemName: "",
+      newItemPrice: 0
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({ name: this.newItemName, price: this.newItemPrice });
+    }
+  }
+};
+</script>
+
+<style>
+.over500 {
+  color: red;
+}
+</style>
